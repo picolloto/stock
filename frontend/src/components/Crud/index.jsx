4752fc0c2ccfd64206162f36bb0193b6d17345e3
Leonardo <leonardopicolloto@gmail.com>
2022-05-29 23:44:43 -0400

    feat: Added compatibility with another pages

--- frontend/src/components/Crud/index.jsx
+++ frontend/src/components/Crud/index.jsx
@@ -1,22 +1,31 @@
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import EditIcon from "@mui/icons-material/Edit";
-import { Button, ButtonGroup, Container, Grid, List, ListItem, ListItemText } from "@mui/material";
+import {
+    Button,
+    ButtonGroup,
+    Container,
+    Grid,
+    List,
+    ListItem,
+    ListItemText,
+    Typography,
+} from "@mui/material";
 import PropTypes from "prop-types";
 import { useMemo } from "react";
 
 import "./Crud.scss";
 
-const Crud = ({ columns, rows, handleEdit, handleDelete }) => {
+const Crud = ({ title, columns, rows, handleEdit, handleDelete, handleNewItem }) => {
     const keys = useMemo(() => {
-        if (rows.length > 0) {
-            return Object.keys(rows[0]);
+        if (columns.length > 0) {
+            return columns.map((row) => row.field);
         }
         return [];
     }, [rows]);
 
     return (
-        <Container>
+        <Container sx={{ marginTop: "20px" }}>
             <List
                 sx={{
                     width: "100%",
@@ -26,57 +35,84 @@ const Crud = ({ columns, rows, handleEdit, handleDelete }) => {
                     padding: "5px",
                 }}
             >
+                <Typography sx={{ paddingBottom: "20px" }} align="center" variant="h4">
+                    {title}
+                </Typography>
                 <ListItem>
                     <Grid container spacing={2} className="grid-container-header">
                         {columns.map((column) => (
-                            <Grid item {...column.col}>
+                            <Grid key={`header-item-${column.field}`} item {...column.col}>
                                 <ListItemText>{column?.label || ""}</ListItemText>
                             </Grid>
                         ))}
                         <Grid item xs={3} lg={2}>
-                            <Button variant="outlined" color="success">
+                            <Button
+                                variant="outlined"
+                                color="success"
+                                onClick={() => handleNewItem()}
+                            >
                                 <AddCircleOutlineIcon /> Adicionar
                             </Button>
                         </Grid>
                     </Grid>
                 </ListItem>
-                {rows.map((row) => (
-                    <ListItem>
-                        <Grid key={`grid-container-${row.id}`} container spacing={2}>
-                            {keys.map((key, index) => {
-                                if (!!columns[index]) {
-                                    return (
-                                        <Grid item key={`row-item-${key}`} {...columns[index]?.col}>
-                                            <ListItemText>{row[key]}</ListItemText>
-                                        </Grid>
-                                    );
-                                }
-                                // eslint-disable-next-line react/jsx-no-useless-fragment
-                                return <></>;
-                            })}
-                            <Grid item xs={3} lg={2}>
-                                <ButtonGroup size="small">
-                                    <Button onClick={() => handleEdit(row)}>
-                                        <EditIcon />
-                                    </Button>
-                                    <Button color="error" onClick={() => handleDelete(row)}>
-                                        <DeleteForeverIcon />
-                                    </Button>
-                                </ButtonGroup>
+                {rows.length > 0 && keys.length > 0 ? (
+                    rows.map((row) => (
+                        <ListItem key={`grid-container-${row.id}`}>
+                            <Grid container spacing={2}>
+                                {keys.map((key, index) => {
+                                    if (!!columns[index]) {
+                                        return (
+                                            <Grid
+                                                item
+                                                key={`row-item-${key}`}
+                                                {...columns[index]?.col}
+                                            >
+                                                <ListItemText>{row[key]}</ListItemText>
+                                            </Grid>
+                                        );
+                                    }
+                                    return null;
+                                })}
+                                <Grid item xs={3} lg={2}>
+                                    <ButtonGroup size="small">
+                                        <Button
+                                            onClick={(e) => {
+                                                e.preventDefault();
+                                                return handleEdit(row);
+                                            }}
+                                        >
+                                            <EditIcon />
+                                        </Button>
+                                        <Button
+                                            color="error"
+                                            onClick={(e) => {
+                                                e.preventDefault();
+                                                return handleDelete(row);
+                                            }}
+                                        >
+                                            <DeleteForeverIcon />
+                                        </Button>
+                                    </ButtonGroup>
+                                </Grid>
                             </Grid>
-                        </Grid>
-                    </ListItem>
-                ))}
+                        </ListItem>
+                    ))
+                ) : (
+                    <Typography align="center">Nenhum registro encontrado.</Typography>
+                )}
             </List>
         </Container>
     );
 };
 
 Crud.propTypes = {
+    title: PropTypes.string.isRequired,
     columns: PropTypes.array.isRequired,
     rows: PropTypes.array.isRequired,
     handleEdit: PropTypes.func.isRequired,
     handleDelete: PropTypes.func.isRequired,
+    handleNewItem: PropTypes.func.isRequired,
 };
 
 export default Crud;
